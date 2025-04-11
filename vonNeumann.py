251512ca22430b5479f8d1dcc16596c917f486ac
Maurillugdar <maurillugdar@gmail.com>
2025-04-10 21:14:15 -0300

    feat: Add main for exec

--- vonNeumann.py
+++ vonNeumann.py
@@ -1,3 +1,10 @@
 def VonNeumann(u: int) -> int:
     u = ((u ** 2) // 100) % 10000
     return u
+
+if __name__ == "__main__":
+    try:
+        import sys
+        print(VonNeumann(int(sys.argv[1])))
+    except ValueError:
+        print("Bad Input")
