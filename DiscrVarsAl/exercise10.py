60e0f4a9b6be9873821edd4621a07211e86165d0
Maurillugdar <maurillugdar@gmail.com>
2025-05-06 10:39:47 -0300

    FEAT: Exercise 10 missing exact E(X)

---
+++ DiscrVarsAl/exercise10.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def accumFunction(n:int)->float:
+    accumVal = sum([(1/2)**(j+1)+(1/2)*(2**(j-1)) / 3**j for j in range(1,n+1)])
+    return accumVal
+
+def massFunction(n:int)->float:
+    return (1/2)**(n+1)+(1/2)*(2**(n-1)) / 3**n
+
+def varDiscAleat()->int:
+    U = np.random.uniform(0,1)
+    i=0
+    F = accumFunction(0)
+    while U >= F:
+        i += 1
+        F += accumFunction(i)
+    return i
+
+if __name__ == "__main__":
+    print(np.mean([varDiscAleat() for _ in range(1000)]))
