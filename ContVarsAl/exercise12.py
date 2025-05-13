453cc50e335bf254f2fb50b7ef9c21903cdb83aa
Maurillugdar <maurillugdar@gmail.com>
2025-05-13 15:56:25 -0300

    FEAT: exerc 12 done

---
+++ ContVarsAl/exercise12.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+def poissonEvents(lam, T):
+    t=0
+    NT=0
+    Events = []
+    while t < T:
+        U = 1- np.random.uniform(0,1)
+        t += -np.log(U)/lam
+        if t <= T:
+            NT += 1
+            Events.append(t)
+    return NT, Events
+
+print(poissonEvents(3,5))
