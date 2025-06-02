2610d42644105ade7ab67bb7265cf0becdca20ff
Maurillugdar <maurillugdar@gmail.com>
2025-06-02 19:29:59 -0300

    FEAT: Implement the idea of the exercise

--- SimData/exercise1.py
+++ SimData/exercise1.py
@@ -2,14 +2,15 @@ from genNormal import normalvariate
 import numpy as np
 
 def exercise1():
-    n = 100
     mu = 0
     sigma = 1
-    while 1:
-        ranNorm = [normalvariate(mu, sigma) for _ in range(n)]
-        S = np.std(a=ranNorm, ddof=1) #ddof indicates that we ar calculating the sample std
-        if S/np.sqrt(n) < 0.1:
+    ranNorm = [normalvariate(mu, sigma), normalvariate(mu, sigma)]
+    n = 2
+    while True:
+        S = np.std(a=ranNorm, ddof=1)
+        if (S/np.sqrt(n) < 0.1) and (n >= 101):
             return n, sum(ranNorm)/n, S ** 2
-        n +=1
+        n += 1
+        ranNorm.append(normalvariate(mu, sigma))
 
 print(exercise1())
