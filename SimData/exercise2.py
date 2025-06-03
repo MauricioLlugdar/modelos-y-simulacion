91cf9d4e5045aceb03563a42b0f9e176017de5c8
Maurillugdar <maurillugdar@gmail.com>
2025-06-03 09:59:35 -0300

    FIX: now working it was not finished

--- SimData/exercise2.py
+++ SimData/exercise2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def aIntegral(u):
-    np.e**u /np.sqrt(2*u)
+    return np.e**u /np.sqrt(2*u)
 
 def exercise2a(Nsim):
     Integral = 0
@@ -10,11 +10,27 @@ def exercise2a(Nsim):
     return Integral / Nsim
 
 def bIntegral(u): # Is an even function as we can see
-    u**2 * np.exp(-u**2)
+    return u**2 * np.exp(-u**2)
 
 def exercise2b(Nsim):
     Integral = 0
     for _ in range(Nsim):
         y = np.random.uniform(0,1)
         Integral += bIntegral(1/y-1) * (1/(y**2))
-    return (Integral / Nsim) # Bcs its an integral from -inf to inf and its and even funct
+    return (Integral / Nsim)*2 # Bcs its an integral from -inf to inf and its and even funct
+
+def sim(func):
+    mean = func(1)
+    n=1
+    Ssq = 0
+    while n<100 or np.sqrt(Ssq/n)>=0.01:
+        predMean = mean
+        mean = predMean + (func(1) - predMean)/(n+1)
+        predSsq = Ssq
+        Ssq = (1-1/n)* predSsq + (n+1)*(mean - predMean)**2
+        n += 1
+    return np.sqrt(Ssq)
+
+print(exercise2a(1000))
+print(exercise2b(1000))
+print(sim(exercise2b))
