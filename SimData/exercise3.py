b2f856e9cb62e364818c420700639fceb6e148af
Maurillugdar <maurillugdar@gmail.com>
2025-06-04 15:35:09 -0300

    FEAT: exercise3 done

---
+++ SimData/exercise3.py
@@ -0,0 +1,44 @@
+from numpy.random import uniform
+from math import pi, sin, sqrt
+from scipy.stats import norm
+
+f_aux_1 = lambda x : sin(x)/x
+f_i = lambda U : pi * f_aux_1(pi + pi*U)
+
+f_aux_2 = lambda x : 3 / (3 + x**4)
+f_ii = lambda U : (1/U**2) * f_aux_2(1/U - 1)
+
+
+z = norm.ppf(0.975) # 1 - alpha/2
+
+def aproxByL(f, Nsim):
+    L = 0.001
+    n, Ssq = 1, 0
+    mean = f(uniform(0,1))
+    while n <= 100 or (sqrt(Ssq / n ) > L)* Nsim or Nsim>n: # If Nsim = 0 => automatic error
+        n += 1
+        predMean = mean
+        mean = predMean + (f(uniform(0,1)) - predMean) / n
+        Ssq = Ssq * (1-1/(n-1)) + n*(mean - predMean)**2
+    return n, mean, Ssq, L
+    
+
+def print_table(results):
+    print(f"{'N° de sim.':>12} | {'Ī':>9} | {'S':>9} | {'IC(95%)':>25}")
+    print("-" * 64)
+    for N, mean, S, IC in results:
+        IC_str = f"[{mean - IC:.4f}, {mean + IC:.4f}]"
+        print(f"{N:>12} | {mean:>9.4f} | {S:>9.4f} | {IC_str:>25}")
+
+results = []
+for f in [f_i, f_ii]:
+    print(f"Monte carlo aproximation for {'f_i' if f == f_i else 'f_ii'}:")
+    print("-" * 64)
+    for N in [1000, 5000, 7000]:
+        n, mean, S, IC = aproxByL(f, N)
+        results.append((N, mean, S, IC))
+    print_table(results)
+    results.clear()
+    print("\n")
+
+
